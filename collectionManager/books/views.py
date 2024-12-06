4221c27fbc1ab7e5976acee4c07d63b2c023c740
asad-yezdan <yezdanasad@gmail.com>
2024-12-06 13:06:19 +0000

    point to home page

--- collectionManager/books/views.py
+++ collectionManager/books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to da home page broski.")
+    return HttpResponse("Welcome to home page broski.")
 
 
 # Create your views here.
