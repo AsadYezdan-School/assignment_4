c3ec7f7d3277662a3142af02ee90f210bec7915f
asad-yezdan <yezdanasad@gmail.com>
2024-12-15 16:51:22 +0000

    Change db setting to prep fpr connection to postgresql db

--- collectionManager/books/views.py
+++ collectionManager/books/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    '''
+    Requiremnets for home page:
+        Register a enw user
+    '''
     return HttpResponse("Welcome to home page broski.")
 
+def queryDB(request):
+    return HttpResponse("Here is where i shall put query results")
+    
 
 # Create your views here.
 
