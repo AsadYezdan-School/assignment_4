6f854612817f7f916cef48e34450d02ba090ccd9
asad-yezdan <yezdanasad@gmail.com>
2024-12-23 20:06:41 +0000

    create test table

--- collectionManager/books/models.py
+++ collectionManager/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class testTable(models.Model):
+    #put the data fields here
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
